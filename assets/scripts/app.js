0c5fe91ae5f1b8bd8e2cef941472f6407bb6fb47
unknown <aszalea_calderon@yahoo.com>
2021-04-15 10:10:32 -0700

    reviewing strings, /n, and back ticks

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,9 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
 currentResult = currentResult + 10;
 
 let calculationDescription = "yo";
+let errorMessage = `An error \n` + `An error occured`;
 
 outputResult(currentResult, calculationDescription);
