67ccc6aef512fbd63c0dfd2b30234ad6e7f2fa4f
unknown <aszalea_calderon@yahoo.com>
2021-04-21 11:40:33 -0700

    adding addition funtion

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {}
+
 currentResult = currentResult + 10;
 
 let calculationDescription = "yo";
-let errorMessage = `An error \n` + `An error occured`;
+let errorMessage = `An error \n` + `An error happened`;
 
 outputResult(currentResult, calculationDescription);
