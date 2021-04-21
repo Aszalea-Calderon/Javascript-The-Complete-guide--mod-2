dd7617c75f5c8c05d4e43cb4ab41a4709742a9d3
unknown <aszalea_calderon@yahoo.com>
2021-04-21 11:42:07 -0700

    cont of adding

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {}
+function add(num1, num2) {
+  const result = num1 + num2;
+}
 
 currentResult = currentResult + 10;
 
