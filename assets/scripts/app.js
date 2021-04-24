bed35a31f779e06b835699775487350813571963
unknown <aszalea_calderon@yahoo.com>
2021-04-24 12:38:33 -0700

    add is there

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,8 @@ let currentResult = defaultResult;
 
 function add() {
   currentResult = currentResult + userInput.value;
+  outputResult(currentResult, calculationDescription);
 }
 addBtn.addEventListener("click", add);
 
-let calculationDescription = `An error \n` + `An error happened`;
-
-outputResult(currentResult, calculationDescription);
+let calculationDescription = `${defaultResult}`;
