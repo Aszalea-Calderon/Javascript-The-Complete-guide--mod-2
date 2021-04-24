321b0a48657f7b5cc0ac44acc098e0070410f7e0
unknown <aszalea_calderon@yahoo.com>
2021-04-24 12:19:04 -0700

    fixing errors

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,13 +5,9 @@ function add(num1, num2) {
   const result = num1 + num2;
   return result;
 }
-add(1, 2);
-add(5, 5);
 
 currentResult = add(2, 3);
 
 let calculationDescription = `An error \n` + `An error happened`;
 
 outputResult(currentResult, calculationDescription);
-/*Remember to always go back to basics*
-asdfasdfasdfasdfasfasfasdfasdf/*
