1990a057e45e5cc4b4fe3bfda8c4e0ba813fdfea
unknown <aszalea_calderon@yahoo.com>
2021-04-29 12:23:26 -0700

    +userInput.value rather than parseInt(userInput.value)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,8 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + userInput.value;
+  currentResult = currentResult + +userInput.value;
+  //You could also convert this to a number by using parseInt(userInput.value) rather than using +userInput.value;
   outputResult(currentResult, calculationDescription);
 }
 addBtn.addEventListener("click", add);
