72ac2550505a9685a31ab9384d4efad05eb36eeb
unknown <aszalea_calderon@yahoo.com>
2021-04-30 11:20:32 -0700

    edits

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
+  const calDescription = `${currentResult} + ${userInput.value}`;
   currentResult = currentResult + +userInput.value;
   //You could also convert this to a number by using parseInt(userInput.value) rather than using +userInput.value;
-  outputResult(currentResult, calculationDescription);
+  outputResult(currentResult, calDescription);
 }
 addBtn.addEventListener("click", add);
-
-let calculationDescription = `${defaultResult}`;
