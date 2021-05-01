64e12a86d3d0d77d1488fdf387a98204ac949833
unknown <aszalea_calderon@yahoo.com>
2021-05-01 10:09:37 -0700

    getUserInput function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  const calDescription = `${currentResult} + ${userInput.value}`;
-  currentResult = currentResult + +userInput.value;
+  const enteredNumber = getUserNumberInput();
+  const calDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + +enteredNumber;
   //You could also convert this to a number by using parseInt(userInput.value) rather than using +userInput.value;
   outputResult(currentResult, calDescription);
 }
